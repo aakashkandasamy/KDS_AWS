095fe6c0c1209d3691d9ce46d601278c34c9415f
aakashkandasamy <67540149+aakashkandasamy@users.noreply.github.com>
2023-03-12 00:08:26 +0800

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,4 +1,14 @@
 import streamlit as st
+import boto3
+import io
+import pyaudio
+from contextlib import closing
+import os
+
+os.environ['AWS_ACCESS_KEY_ID'] = 'AKIAQFHL5JQLK77ASDF3'
+os.environ['AWS_SECRET_ACCESS_KEY'] = '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'
+
+
 
 st.set_page_config(page_title='Image Uploader', page_icon=':camera:')
 
@@ -8,3 +18,13 @@ file = st.file_uploader('Choose an image file', type=['jpg', 'jpeg', 'png'])
 
 if file is not None:
     st.image(file, caption='Uploaded Image')
+    polly = boto3.client('polly', region_name='us-east-1')
+    text = "Your file has been uploaded!"
+    voice_id = 'Raveena'
+    output_format = 'pcm'
+    response = polly.synthesize_speech(Text=text, VoiceId=voice_id, OutputFormat=output_format)
+    audio_bytes = response['AudioStream'].read()
+    pa = pyaudio.PyAudio()
+    stream = pa.open(format=pyaudio.paInt16, channels=1, rate=16000, output=True)
+    with closing(stream):
+        stream.write(audio_bytes)
